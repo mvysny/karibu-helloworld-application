99dcace69bb854d9c56eb6a76beb01d9a1c84f5c
Martin Vysny <mavi@vaadin.com>
2018-02-17 20:49:13 +0200

    Switched tests to DynaTest

--- src/test/kotlin/com/vaadin/flow/demo/helloworld/MainViewTest.kt
+++ src/test/kotlin/com/vaadin/flow/demo/helloworld/MainViewTest.kt
@@ -4,22 +4,15 @@ import com.github.karibu.testing.MockVaadin
 import com.github.karibu.testing._click
 import com.github.karibu.testing._get
 import com.github.karibu.testing.autoDiscoverViews
+import com.github.mvysny.dynatest.DynaTest
 import com.vaadin.flow.component.button.Button
-import com.vaadin.flow.component.html.Label
-import com.vaadin.flow.component.textfield.TextField
-import org.junit.Before
-import org.junit.Test
 import kotlin.test.expect
 
-class MainViewTest {
-    @Before
-    fun mock() {
-        MockVaadin.setup(autoDiscoverViews("com.vaadin.flow.demo"))
-    }
+class MainViewTest: DynaTest({
+    beforeEach { MockVaadin.setup(autoDiscoverViews("com.vaadin.flow.demo")) }
 
-    @Test
-    fun testGreeting() {
+    test("test greeting") {
         _get<Button> { caption = "Click me" } ._click()
         expect("Clicked!") { _get<ExampleTemplate>().value }
     }
-}
+})
