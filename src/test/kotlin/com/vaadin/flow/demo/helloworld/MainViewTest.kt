3557f57faad1767c100ff98af673570efe8de388
Martin Vysny <vysny@baka.sk>
2018-02-27 21:09:54 +0200

    MainViewTest: added doc

--- src/test/kotlin/com/vaadin/flow/demo/helloworld/MainViewTest.kt
+++ src/test/kotlin/com/vaadin/flow/demo/helloworld/MainViewTest.kt
@@ -8,11 +8,18 @@ import com.github.mvysny.dynatest.DynaTest
 import com.vaadin.flow.component.button.Button
 import kotlin.test.expect
 
+/**
+ * Tests the UI. Uses the Serverless testing approach as provided by the [Karibu Testing](https://github.com/mvysny/karibu-testing) library.
+ */
 class MainViewTest: DynaTest({
     beforeEach { MockVaadin.setup(autoDiscoverViews("com.vaadin.flow.demo")) }
 
     test("test greeting") {
+        // MockVaadin.setup() discovered all @Routes and prepared the UI for us; we can now read components from it.
+        // simulate a button click as if clicked by the user
         _get<Button> { caption = "Click me" } ._click()
+
+        // look up the Example Template and assert on its value
         expect("Clicked!") { _get<ExampleTemplate>().value }
     }
 })
