0f46e2a41a6456c12324a8405419736e87aa88f1
Martin Vysny <martin@vysny.me>
2018-10-17 10:16:10 +0300

    Gradle: port to Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,73 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val karibudsl_version = "0.4.11"
+val vaadin10_version = "11.0.1"
+
+plugins {
+    kotlin("jvm") version "1.2.71"
+    id("org.gretty") version "2.2.0"
+    id("io.spring.dependency-management") version "1.0.6.RELEASE"  // remove when https://github.com/gradle/gradle/issues/4417 is fixed
+    war
+}
+
+defaultTasks("clean", "build")
+
+repositories {
+    jcenter()
+    maven { setUrl("https://dl.bintray.com/mvysny/github") }
+}
+
+gretty {
+    contextPath = "/"
+    servletContainer = "jetty9.4"
+}
+
+dependencyManagement {
+    imports { mavenBom("com.vaadin:vaadin-bom:$vaadin10_version") }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        // to see the exceptions of failed tests in Travis-CI console.
+        exceptionFormat = TestExceptionFormat.FULL
+    }
+}
+
+dependencies {
+    // Karibu-DSL dependency, includes Vaadin
+    compile("com.github.vok.karibudsl:karibu-dsl-v10:$karibudsl_version")
+    providedCompile("javax.servlet:javax.servlet-api:3.1.0")
+
+    // logging
+    // currently we are logging through the SLF4J API to LogBack. See src/main/resources/logback.xml file for the logger configuration
+    compile("ch.qos.logback:logback-classic:1.2.3")
+    compile("org.slf4j:slf4j-api:1.7.25")
+
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // test support
+    testCompile("com.github.kaributesting:karibu-testing-v10:0.4.17")
+    testCompile("com.github.mvysny.dynatest:dynatest-engine:0.11")
+
+    // heroku app runner
+    testRuntime("com.github.jsimone:webapp-runner:9.0.11.0")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+// Heroku
+tasks {
+    val copyToLib by registering(Copy::class) {
+        into("$buildDir/server")
+        from(configurations.testRuntime) {
+            include("webapp-runner*")
+        }
+    }
+    val stage by registering {
+        dependsOn("build", copyToLib)
+    }
+}
