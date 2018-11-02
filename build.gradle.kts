8024de5c184fc19683ec706d807c90a27b841374
Martin Vysny <martin@vysny.me>
2018-11-02 19:57:07 +0200

    v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val karibudsl_version = "0.4.11"
+val karibudsl_version = "0.4.12"
 val vaadin10_version = "11.0.1"
 
 plugins {
