86fdac95e6e17eb4d7e6ae62ca0a08f9fd4d4de8
Martin Vysny <vysny@baka.sk>
2019-02-10 10:29:36 +0200

    v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val karibudsl_version = "0.5.3"
-val vaadin10_version = "12.0.4"
+val vaadin10_version = "12.0.5"
 
 plugins {
-    kotlin("jvm") version "1.3.20"
+    kotlin("jvm") version "1.3.21"
     id("org.gretty") version "2.2.0"
     war
 }
@@ -46,7 +46,7 @@ dependencies {
     compile(kotlin("stdlib-jdk8"))
 
     // test support
-    testCompile("com.github.mvysny.kaributesting:karibu-testing-v10:1.0.6")
+    testCompile("com.github.mvysny.kaributesting:karibu-testing-v10:1.0.7")
     testCompile("com.github.mvysny.dynatest:dynatest-engine:0.13")
 
     // heroku app runner
