f932d0e3a395599a4cc277d89e051a596157c488
Martin Vysny <mavi@vaadin.com>
2020-01-27 08:23:17 +0200

    v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val karibudsl_version = "0.7.4"
-val vaadin_version = "14.1.3"
+val vaadin_version = "14.1.5"
 
 plugins {
     kotlin("jvm") version "1.3.61"
