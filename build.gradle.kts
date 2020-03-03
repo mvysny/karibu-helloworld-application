fedebdb854f5c035721a56e6e3d79d2028cf38f3
Martin Vysny <mavi@vaadin.com>
2020-03-03 14:00:17 +0200

    minor

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,10 +34,10 @@ val staging by configurations.creating
 
 dependencies {
     // Karibu-DSL dependency
-    compile("com.github.mvysny.karibudsl:karibu-dsl-v10:$karibudsl_version")
+    implementation("com.github.mvysny.karibudsl:karibu-dsl-v10:$karibudsl_version")
 
     // Vaadin 14
-    compile("com.vaadin:vaadin-core:${vaadin_version}") {
+    implementation("com.vaadin:vaadin-core:${vaadin_version}") {
         // Webjars are only needed when running in Vaadin 13 compatibility mode
         listOf("com.vaadin.webjar", "org.webjars.bowergithub.insites",
                 "org.webjars.bowergithub.polymer", "org.webjars.bowergithub.polymerelements",
@@ -48,13 +48,13 @@ dependencies {
 
     // logging
     // currently we are logging through the SLF4J API to SLF4J-Simple. See src/main/resources/simplelogger.properties file for the logger configuration
-    compile("org.slf4j:slf4j-simple:1.7.30")
+    implementation("org.slf4j:slf4j-simple:1.7.30")
 
-    compile(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib-jdk8"))
 
     // test support
-    testCompile("com.github.mvysny.kaributesting:karibu-testing-v10:1.1.19")
-    testCompile("com.github.mvysny.dynatest:dynatest-engine:0.16")
+    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.1.19")
+    testImplementation("com.github.mvysny.dynatest:dynatest-engine:0.16")
 
     // heroku app runner
     staging("com.github.jsimone:webapp-runner-main:9.0.27.1")
