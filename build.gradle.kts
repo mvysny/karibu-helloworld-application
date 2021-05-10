0ba62e5a812626287614fb671cb30cc5c963a5d2
Martin Vysny <mavi@vaadin.com>
2021-05-10 16:24:29 +0300

    vaadin v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,14 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.32"
+    kotlin("jvm") version "1.5.0"
     id("org.gretty") version "3.0.4"
     war
     id("com.vaadin") version "0.14.5.1"
 }
 
 val karibudsl_version = "1.0.6"
-val vaadin_version = "14.5.3"
+val vaadin_version = "14.5.5"
 
 defaultTasks("clean", "build")
 
