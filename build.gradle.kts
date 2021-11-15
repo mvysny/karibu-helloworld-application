6fc9a1e57bbd97b5732fc075cd9ccbdffadf198d
Martin Vysny <mavi@vaadin.com>
2021-11-15 12:58:27 +0200

    dependency v-bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
     // test support
-    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.3.6")
+    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v10:1.3.7")
     testImplementation("com.github.mvysny.dynatest:dynatest:0.22")
 
     // heroku app runner
