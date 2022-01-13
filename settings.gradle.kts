be8b15c7bd09415448fd7333b55dca99599cd077
Martin Vysny <mavi@vaadin.com>
2022-01-13 09:37:23 +0200

    minor

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,7 @@
 pluginManagement {
+    // also download plugins from the vaadin-prereleases repository.
+    // this will allow us to use alpha builds of Vaadin Gradle plugin, since those
+    // are not published to https://plugins.gradle.org/plugin/com.vaadin
     repositories {
         maven { setUrl("https://maven.vaadin.com/vaadin-prereleases") }
         gradlePluginPortal()
