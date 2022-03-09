01ca19913808a8ad8e3ac7a9c7890ddfb428b37d
Martin Vysny <mavi@vaadin.com>
2022-03-09 23:08:08 +0200

    vaadin v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
     kotlin("jvm") version "1.6.10"
     id("org.gretty") version "3.0.6"
     war
-    id("com.vaadin") version "23.0.0"
+    id("com.vaadin") version "23.0.1"
 }
 
 val karibudsl_version = "1.1.1"
-val vaadin_version = "23.0.0"
+val vaadin_version = "23.0.1"
 
 defaultTasks("clean", "build")
 
