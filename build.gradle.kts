f361677c26ee50a05b598a289fc820753e0bfa67
Martin Vysny <mavi@vaadin.com>
2022-08-08 08:53:49 +0300

    vaadin v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,14 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version "1.7.10"
     id("org.gretty") version "3.0.6"
     war
-    id("com.vaadin") version "23.1.1"
+    id("com.vaadin") version "23.1.4"
 }
 
 val karibudsl_version = "1.1.3"
-val vaadin_version = "23.1.1"
+val vaadin_version = "23.1.4"
 
 defaultTasks("clean", "build")
 
@@ -50,7 +50,7 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
     // test support
-    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v23:1.3.16")
+    testImplementation("com.github.mvysny.kaributesting:karibu-testing-v23:1.3.17")
     testImplementation("com.github.mvysny.dynatest:dynatest:0.24")
 
     // heroku app runner
