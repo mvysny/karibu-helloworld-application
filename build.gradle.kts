6f6976a1210fa3a4fb263cb49c0d45e7c23cdcb7
Martin Vysny <mavi@vaadin.com>
2022-10-28 19:20:04 +0300

    dep v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
 
     // Vaadin
     implementation("com.vaadin:vaadin-core:${vaadin_version}")
-    implementation("com.github.mvysny.vaadin-boot:vaadin-boot:10.0")
+    implementation("com.github.mvysny.vaadin-boot:vaadin-boot:10.1")
 
     // logging
     // currently we are logging through the SLF4J API to SLF4J-Simple. See src/main/resources/simplelogger.properties file for the logger configuration
