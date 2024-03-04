7e0fc6beef90c6642436217c5c70bae2c99cc946
Martin Vysny <mavi@vaadin.com>
2024-03-04 12:12:19 +0200

    vaadin v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,7 @@ dependencies {
 
     // Vaadin
     implementation(libs.vaadin.core) {
-        // https://github.com/vaadin/flow/issues/18572
-        if (vaadin.productionMode.map { v -> getBooleanProperty("vaadin.productionMode") ?: v }.get()) {
+        if (vaadin.effective.productionMode.get()) {
             exclude(module = "vaadin-dev")
         }
     }
