4fd909e798d36136135ec871b5ed72a254917c83
Martin Vysny <mavi@vaadin.com>
2025-10-26 12:08:59 +0200

    formatting

--- src/main/kotlin/com/example/karibudsl/Main.kt
+++ src/main/kotlin/com/example/karibudsl/Main.kt
@@ -7,7 +7,7 @@ import com.vaadin.flow.theme.Theme
 
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
 @Theme("my-theme")
-class AppShell: AppShellConfigurator
+class AppShell : AppShellConfigurator
 
 /**
  * Run this function to launch your app in Embedded Jetty.
