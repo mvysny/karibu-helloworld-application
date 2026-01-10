19a9d51a490b53ad75b340d6524cf42925fc3f0a
Martin Vysny <mavi@vaadin.com>
2026-01-10 15:50:28 +0200

    upgrade styles; bundle no longer necessary

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,15 @@
 package com.example.karibudsl
 
 import com.github.mvysny.vaadinboot.VaadinBoot
+import com.vaadin.flow.component.dependency.StyleSheet
 import com.vaadin.flow.component.page.AppShellConfigurator
 import com.vaadin.flow.server.PWA
 import com.vaadin.flow.theme.Theme
+import com.vaadin.flow.theme.lumo.Lumo
 
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
-@Theme("my-theme")
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet("styles.css")
 class AppShell : AppShellConfigurator
 
 /**
