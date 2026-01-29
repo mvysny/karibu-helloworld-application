c2c58355c1e4bec463728c0bb73082b76f80b253
Martin Vysny <mavi@vaadin.com>
2026-01-29 08:31:27 +0200

    kotlin v bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.2.21"
+    kotlin("jvm") version "2.3.0"
     application
     alias(libs.plugins.vaadin)
 }
